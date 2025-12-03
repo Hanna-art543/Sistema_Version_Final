8c1318f17bdd546985bf7db732fb8026358df4dd
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-12-02 21:21:03 -0500

    Refactor CursoTeorico and CursoPractico classes

--- CursoTeorico.java
+++ CursoTeorico.java
@@ -29,33 +29,3 @@ public class CursoTeorico extends Curso {
     }
 }
 
-public class CursoPractico extends Curso {
-    private double notaLaboratorio;
-    private double notaProyectoFinal;
-
-    public CursoPractico(String nombre, String codigo, int creditos, int semestre) {
-        super(nombre, codigo, creditos, semestre);
-    }
-
-    // Setters 
-    public void setNotaLaboratorio(double notaLaboratorio) { 
-      this.notaLaboratorio = notaLaboratorio; 
-    }
-    public void setNotaProyectoFinal(double notaProyectoFinal) { 
-      this.notaProyectoFinal = notaProyectoFinal; 
-    }
-
-    @Override
-    public double calcularNotaFinal() {
-        // Ejemplo de lógica: 60% laboratorio, 40% proyecto
-        return (notaLaboratorio * 0.6) + (notaProyectoFinal * 0.4);
-    }
-
-    @Override
-    public void mostrarInformacion() {
-        super.mostrarInformacion();
-        System.out.println("Tipo: Práctico");
-        System.out.println("Nota Laboratorio: " + notaLaboratorio + ", Nota Proyecto: " + notaProyectoFinal);
-        System.out.println("Promedio Final: " + calcularNotaFinal());
-    }
-}
