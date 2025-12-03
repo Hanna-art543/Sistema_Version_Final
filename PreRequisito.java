46508e6f1b9323c054035acdf1a4fcf68d579ebf
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-12-02 21:53:29 -0500

    Refactor verificarCumplimiento method and add getters

--- PreRequisito.java
+++ PreRequisito.java
@@ -1,22 +1,34 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class PreRequisito {
+    // ATRIBUTOS
     private String ID;
     private double notaMinima;
     private Curso cursoRequerido;
 
+    // CONSTRUCTOR DE INSTANCIA
     public PreRequisito(String ID, double notaMinima, Curso cursoRequerido) {
         this.ID = ID;
         this.notaMinima = notaMinima;
         this.cursoRequerido = cursoRequerido;
     }
 
-    public boolean verificarCumplimiento(Alumno a) {
-        return false;
+    // GETTERS
+    public String getId() { return ID; }
+    public Curso getCursoRequerido() { return cursoRequerido; }
+
+    // FUNCION PARA VERIFICAR CUMPLIMIENTO
+    public boolean verificarCumplimiento(Alumno a, GestorNotas gestor) { 
+        if (a == null || cursoRequerido == null || gestor == null) {
+            return false;
+        }
+        
+        double promedio = gestor.calcularPromedio(a, cursoRequerido);
+        
+        return promedio >= notaMinima;
     }
 
     public void mostrarInformacion() {
-        System.out.println(ID);
+        System.out.println("PreRequisito [" + ID + "]: Aprobar " + cursoRequerido.getNombre() + " con nota mínima de " + notaMinima);
     }
 }
