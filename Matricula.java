1717f01648262168579d471c9cd2c4457c1cfb9e
Hanna-art543 <amirandalo@unsa.edu.pe>
2025-12-02 21:50:45 -0500

    Rename class 'matricula' to 'Matricula'

--- Matricula.java
+++ Matricula.java
@@ -1,22 +1,38 @@
-public abstract class matricula {
+public abstract class Matricula {
     protected String ID;
     protected String fecha;
     protected boolean estado;
     protected Alumno alumno;
-    public matricula(String ID, String fecha, Alumno alumno) {
+    public Matricula(String ID, String fecha, Alumno alumno) {
         this.ID = ID;
         this.fecha = fecha;
         this.alumno = alumno;
         this.estado = false; 
     }
-    public String getID() { return ID; }
-    public void setID(String ID) { this.ID = ID; }
-    public String getFecha() { return fecha; }
-    public void setFecha(String fecha) { this.fecha = fecha; }
-    public boolean isEstado() { return estado; }
-    public void setEstado(boolean estado) { this.estado = estado; }
-    public Alumno getAlumno() { return alumno; }
-    public void setAlumno(Alumno alumno) { this.alumno = alumno; }
+    public String getID() { 
+        return ID; 
+    }
+    public void setID(String ID) { 
+        this.ID = ID; 
+    }
+    public String getFecha() { 
+        return fecha; 
+    }
+    public void setFecha(String fecha) { 
+        this.fecha = fecha; 
+    }
+    public boolean isEstado() { 
+        return estado; 
+    }
+    public void setEstado(boolean estado) { 
+        this.estado = estado; 
+    }
+    public Alumno getAlumno() { 
+        return alumno; 
+    }
+    public void setAlumno(Alumno alumno) { 
+        this.alumno = alumno; 
+    }
     public abstract void registrarMatricula(Alumno a, Carrera c);
     public void cancelarMatricula() {
         if (estado) {
